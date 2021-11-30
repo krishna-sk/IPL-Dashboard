62930136da1e0b141e47454dbf53d5d5dde38f87
krishna <krishna.sk7991@gmail.com>
2021-11-30 20:34:32 +0530

    enpoint to fetch matches and matchpage routing

--- back-end/src/main/java/com/ipl/repository/MatchRepository.java
+++ back-end/src/main/java/com/ipl/repository/MatchRepository.java
@@ -1,16 +1,33 @@
 package com.ipl.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.ipl.model.Match;
 
 public interface MatchRepository extends JpaRepository<Match, Long> {
 
 	List<Match> getByTeam1OrTeam2OrderByDateDesc(String team1, String team2, Pageable pageable);
+
+	/*
+	 * List<Match>
+	 * getByTeam1AndDateBetweenOrTeam2AndDateBetweenOrderByDateDesc(String team1,
+	 * LocalDate startDate, LocalDate endDate, String team2, LocalDate startDate1,
+	 * LocalDate endDate1);
+	 */
+
+	 @Query("select m from Match m where (m.team1 = :teamName or m.team2 = :teamName) and m.date between :dateStart and :dateEnd order by date desc")
+	    List<Match> getMatchesByTeamBetweenDates(
+	        @Param("teamName") String teamName, 
+	        @Param("dateStart") LocalDate dateStart, 
+	        @Param("dateEnd") LocalDate dateEnd
+	    );
 
 	default List<Match> findLatestMatchesByTeam(String team, int count) {
 		return getByTeam1OrTeam2OrderByDateDesc(team, team, PageRequest.of(0, count));
--- front-end/src/components/pages/MatchPage.js
+++ front-end/src/components/pages/MatchPage.js
@@ -1,9 +1,31 @@
 import { React } from "react";
+import { useState, useEffect } from "react";
+import { MatchDetailCard } from "./MatchDetailCard";
+import { useParams } from "react-router";
 
 export const MatchPage = () => {
+  const [matches, setmatches] = useState([]);
+
+  const { teamName, year } = useParams();
+
+  useEffect(() => {
+    const fetchMatches = async () => {
+      const response = await fetch(
+        `http://localhost:8080/ipl/team/${teamName}/matches?year=${year}`
+      );
+      const data = await response.json();
+      setmatches(data);
+    };
+
+    fetchMatches();
+  }, [teamName, year]);
+
   return (
     <div className="MatchPage">
       <h1>MatchPage</h1>
+      {matches.map((match) => (
+        <MatchDetailCard teamName={teamName} match={match} />
+      ))}
     </div>
   );
 };
